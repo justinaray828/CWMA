fbd5423d91431ebdfdd4df00b47426f585918e33
justinaray828 <justinaray828@gmail.com>
2019-02-03 22:56:35 -0500

    Added some scritable objects but not much has been implemented yet.

---
+++ Assets/Scripts/Names.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class to store all names to keep consistent and to allow easy updating if it changes
+/// </summary>
+public static class Names
+{
+    public static string mainCharacterName = "Jerry";
+    public static string dateName = "Lisa";
+    public static string uberDriverName = "Akmed";
+}

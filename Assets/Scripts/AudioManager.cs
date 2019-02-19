94aef753c62d4225a9cd1dac1ba4ebf03a9b478b
jaltman <joshuaealtman@gmail.com>
2019-02-19 12:53:42 -0500

    audio Manager singleton initial

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

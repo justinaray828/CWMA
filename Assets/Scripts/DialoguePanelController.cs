75b26fca9f5b4e31a17256b9c6292008da2fc923
Joshua Altman <joshuaealtman@gmail.com>
2019-07-20 13:02:18 -0400

    Dialogue panel is now dynamic. Added animations, and fixed auto-button-size errors

--- Assets/Scripts/DialoguePanelController.cs
+++ Assets/Scripts/DialoguePanelController.cs
@@ -10,7 +10,7 @@ public class DialoguePanelController : MonoBehaviour
 
     public Text nameText;
     public Text dialogueText;
-    //public Animator animator;
+    public Animator animator;
 
     private float smoothTime = 0.3f;
     private float yVelocity = 0.3f;
@@ -22,30 +22,10 @@ public class DialoguePanelController : MonoBehaviour
     /// <summary>
     /// Set to true to pop up panel and false to bring it down.
     /// </summary>
-    private bool popUp = false;
-
     public bool PopUp
     {
-        get
-        {
-            return popUp;
-        }
-        set
-        {
-            popUp = value;
-            DialoguePanelPopUp(popUp);
-        }
-    }
-
-    private void Start()
-    {
-        panelLocation = lowestPanelLocation;
-        //OpenDialoguePanel();
-    }
-
-    private void FixedUpdate()
-    {
-        MovePanel();
+        get => animator.GetBool("IsOpen");
+        set => animator.SetBool("IsOpen", value);
     }
 
     public void AdvanceDialogue(string speakerName, string speakerText)
@@ -55,39 +35,6 @@ public class DialoguePanelController : MonoBehaviour
 
         nameText.text = speakerName + ":";
         dialogueText.text = speakerText;
-    }
-
-    public void OpenDialoguePanel()
-    {
-        //animator.SetBool("IsOpen", true);
-    }
-
-    public void CloseDialoguePanel()
-    {
-       // animator.SetBool("IsOpen", false);
-    }
-
-    /// <summary>
-    /// Call to pop up window. True is up and false is down.
-    /// </summary>
-    /// <param name="popUp"></param>
-    private void DialoguePanelPopUp(bool popUp)
-    {
-        if (popUp)
-        {
-            panelLocation = highestPanelLocation;
-        }
-        else
-        {
-            panelLocation = lowestPanelLocation;
-        }
-    }
-
-    private void MovePanel()
-    {
-        float newPosition;
-        newPosition = Mathf.SmoothDamp(dialoguePanelRectTransform.position.y, panelLocation, ref yVelocity, smoothTime);
-        dialoguePanelRectTransform.position = new Vector2(dialoguePanelRectTransform.position.x, newPosition);
     }
 
     private void ToggleDialogueText(bool textState)

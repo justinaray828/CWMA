fcca59df2181e506e0781b784b47370646786e11
justinaray828 <justinaray828@gmail.com>
2019-08-24 15:55:11 -0400

    - Fixed animations for restaurant - Updated restaurant yarn script for talking and animations

---
+++ Assets/ResturantAnimation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+[RequireComponent(typeof(Animator))]
+public class ResturantAnimation : MonoBehaviour
+{
+    private Animator restaurantAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        restaurantAnimator = GetComponent<Animator>();
+        SetReaLeavesBool(true);
+    }
+
+    [YarnCommand("GinaLeaves")]
+    public void GinaLeaves()
+    {
+        SetGinaLeavesBool(true);
+    }
+
+    [YarnCommand("GinaReturns")]
+    public void GinaReturns()
+    {
+        SetGinaLeavesBool(false);
+    }
+
+    [YarnCommand("ReaLeaves")]
+    public void ReaLeaves()
+    {
+        SetReaLeavesBool(true);
+    }
+
+    [YarnCommand("ReaReturns")]
+    public void ReaReturns()
+    {
+        SetReaLeavesBool(false);
+    }
+
+    private void SetGinaLeavesBool(bool boolean)
+    {
+        restaurantAnimator.SetBool("GinaLeaving", boolean);
+    }
+
+    private void SetReaLeavesBool(bool boolean)
+    {
+        restaurantAnimator.SetBool("ReaLeaving", boolean);
+    }
+}

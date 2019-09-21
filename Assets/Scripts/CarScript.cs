16ea612d6c971059aa7f596dabbe1fa3e51c1b7b
JoshAltman <joshuaealtman@gmail.com>
2019-09-21 16:42:43 -0400

    add car stop and go functionality

---
+++ Assets/Scripts/CarScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using Yarn.Unity;
+
+public class CarScript : MonoBehaviour
+{
+    private VideoPlayer vp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vp = GetComponentInChildren<VideoPlayer>();
+    }
+
+    [YarnCommand("CarStop")]
+    public void CarStop()
+    {
+        vp.Pause();
+    }
+
+    [YarnCommand("CarGo")]
+    public void CarGo()
+    {
+        vp.Play();
+    }
+}

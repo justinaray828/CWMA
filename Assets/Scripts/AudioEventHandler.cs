dc0bda15106d9306f99e7636378e9dbed7d59b2f
Joshua Altman <joshuaealtman@gmail.com>
2019-10-30 21:16:05 -0400

    more sounds and loops

--- Assets/Scripts/AudioEventHandler.cs
+++ Assets/Scripts/AudioEventHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -29,11 +30,30 @@ public class AudioEventHandler : MonoBehaviour
         {
             Setup_1stCar();
         }
-        Debug.Log(SceneManager.GetActiveScene().name);
-
+        //Debug.Log(SceneManager.GetActiveScene().name);
         if (SceneManager.GetActiveScene().name == "03_Theater")
         {
             Setup_Theater();
+        }
+        if (SceneManager.GetActiveScene().name == "04_2ndCar")
+        {
+            Setup_2ndCar();
+        }
+        if (SceneManager.GetActiveScene().name == "05_Resturant")
+        {
+            Setup_Resturant();
+        }
+        if (SceneManager.GetActiveScene().name == "06_Ending_SecondDate")
+        {
+            Setup_SecondDate();
+        }
+        if (SceneManager.GetActiveScene().name == "07_Ending_Jordy")
+        {
+            Setup_Jordy();
+        }
+        if (SceneManager.GetActiveScene().name == "08_Ending_Alone")
+        {
+            Setup_Alone();
         }
     }
 
@@ -41,19 +61,17 @@ public class AudioEventHandler : MonoBehaviour
     {
         EventManager.StopListening("Pause", Pause);
         EventManager.StopListening("Unpause", Unpause);
-        if (SceneManager.GetActiveScene().name == "02_1stCar")
-        {
-            EventManager.StopListening("Car_go", Car_go);
-            EventManager.StopListening("Car_stop", Car_stop);
-            EventManager.StopListening("EnterBrainRoom", EnterBrainRoom);
-            EventManager.StopListening("ExitBrainRoom", ExitBrainRoom);
-            EventManager.StopListening("brainOpen", BrainOpen);
-            EventManager.StopListening("brainClose", BrainClose);
-        }
+        EventManager.StopListening("Car_go", Car_go);
+        EventManager.StopListening("Car_stop", Car_stop);
+        EventManager.StopListening("EnterBrainRoom", EnterBrainRoom);
+        EventManager.StopListening("ExitBrainRoom", ExitBrainRoom);
+        EventManager.StopListening("brainOpen", BrainOpen);
+        EventManager.StopListening("brainClose", BrainClose);
     }
 
     private void Setup_1stCar()
     {
+        am.FadeInSFX("car_loop");
         EventManager.StartListening("Car_go", Car_go);
         EventManager.StartListening("Car_stop", Car_stop);
         EventManager.StartListening("EnterBrainRoom", EnterBrainRoom);
@@ -62,14 +80,42 @@ public class AudioEventHandler : MonoBehaviour
         EventManager.StartListening("brainClose", BrainClose);
     }
 
-    private void SceneChange()
-    {
-        am.SceneEnd();
-    }
-
     private void Setup_Theater()
     {
         am.FadeInSFX("theater_lobby_loop");
+    }
+
+    private void Setup_2ndCar()
+    {
+        am.FadeInSFX("car_loop");
+        EventManager.StartListening("Car_go", Car_go);
+        EventManager.StartListening("Car_stop", Car_stop);
+        EventManager.StartListening("EnterBrainRoom", EnterBrainRoom);
+        EventManager.StartListening("ExitBrainRoom", ExitBrainRoom);
+        EventManager.StartListening("brainOpen", BrainOpen);
+        EventManager.StartListening("brainClose", BrainClose);
+    }
+
+    private void Setup_Alone()
+    {
+    }
+
+    private void Setup_Jordy()
+    {
+    }
+
+    private void Setup_SecondDate()
+    {
+    }
+
+    private void Setup_Resturant()
+    {
+        am.FadeInSFX("restaurant_loop");
+    }
+
+    private void SceneChange()
+    {
+        am.SceneEnd();
     }
 
     private void Car_go()

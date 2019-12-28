7001db5b50167aa44bd13b10558b9c0d3aa24780
Joshua Altman <joshuaealtman@gmail.com>
2019-12-28 16:28:15 -0500

    more imports

---
+++ Assets/Scripts/backgroundcylinder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundcylinder : MonoBehaviour
+{
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, -Time.deltaTime * speed, 0, Space.Self);
+    }
+}

4ab71bc509b6507db5ffe3909e5b9d4e91276c62
justinaray828 <justinaray828@gmail.com>
2019-12-28 10:04:25 -0500

    - Added cellphone start screen - Added script to handle sending text - Added settings cellphone - Updated sliders so that they look good on phone screen

---
+++ Assets/TitleButtonPress.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleButtonPress : MonoBehaviour
+{
+    SceneHandler sceneHandler;
+    string carScene = "02_1stCar";
+    string optionsScene = "01b_Options";
+    string menuScene = "01a_StartUp"; //TODO: Need to implement
+
+    public enum clickActions { Start, Options, Exit};
+    public clickActions clickAction;
+
+    private void Start()
+    {
+        sceneHandler = FindObjectOfType<SceneHandler>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch (clickAction)
+        {
+            case clickActions.Start:
+                sceneHandler.LoadScene(carScene);
+                break;
+            case clickActions.Options:
+                sceneHandler.LoadScene(optionsScene);
+                break;
+            case clickActions.Exit:
+                sceneHandler.ExitGame();
+                break;
+            default:
+                Debug.Log("Incorrect Action");
+                break;
+        }
+
+    }
+}
